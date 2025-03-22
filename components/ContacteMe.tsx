98d49eacde0b9611f4d179ccb68d8afe934b5b31
John Rodrigues <john.rodrigues@gmail.com>
2025-03-22 05:54:38 -0300

    feat: Ajuste do contato do mobile

--- components/ContacteMe.tsx
+++ components/ContacteMe.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 
 export function ContacteMe() {
   return (
-    <div className="h-[400px] flex flex-col justify-center gap-4 mt-5 p-5 md:p-0">
+    <div className="h-[400px] flex flex-col justify-center gap-4 p-5 pt-0 mb-8 md:p-0">
       <h1 className="text-2xl font-bold">Contact Me</h1>
       <p className="text-base text-gray-700 dark:text-gray-300">
         Estou à disposição para conversar sobre oportunidades e esclarecer qualquer dúvida. Sinta-se à vontade para entrar em contato por mensagem direta no
